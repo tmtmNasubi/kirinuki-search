0cf67b61518c77efccd04b7eb95ab5472648cd9d
tmtmNasubi <tamaoki.tmtm@gmail.com>
2026-05-25 23:17:40 +0900

    feat: Google analytics

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,7 @@
 /// <reference types="@cloudflare/workers-types" />
 
 interface ImportMetaEnv {
+  readonly VITE_GA_MEASUREMENT_ID?: string;
   readonly VITE_TURNSTILE_SITE_KEY?: string;
   readonly VITE_USE_REAL_SEARCH?: string;
 }
